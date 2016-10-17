1218e363c1531114777e8cb309bee00d9c124f47
KB park <kb.coille@gmail.com>
2016-10-17 00:57:28 +0000

    test commit

---
+++ HelloWebServer/app.js
@@ -0,0 +1,17 @@
+var express = require("express")
+  , http = require("http")
+  , app = express()
+  ,server = http.createServer(app);
+  
+  app.get(`/`, function (req, res) {
+      res.send(`Hello /`);
+  });
+  
+  app.get(`/world.html`, function (req, res) {
+      res.send(`Hello World`);
+  });
+  
+  server.listen(process.env.PORT, function() {
+      console.log(`Express server listening on port` + server.address().prot);
+  }) ;
+
--- server.js
+++ server.js
@@ -7,6 +7,6 @@ function onRequest(request, response) {
   response.end();
 }
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(8000);
 
 console.log("Server has started.");
